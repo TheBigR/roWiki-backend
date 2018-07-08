b50f4ce37c8c525cfa7d7798dabfaca743b93696
thebigr <nevoroy@gmail.com>
2018-07-08 10:45:19 +0300

    db.js - loaded with pages. server is starting to build up.

---
+++ db.js
@@ -0,0 +1,16 @@
+const PAGES = [
+    {id: 11, title: 'the big R', body: "my very first article. another one <b>bites</b> the dust..."},
+    {id: 12, title: 'magical L', body: "made me the luckiest person alive. <img src=\"http://jeeptrip.co.il/Files/pages/img_2029.jpg\">"},
+    {id: 13, title: 'number 1 A', body: 'simply the best.'},
+    {id: 14, title: 'M A ', body: 'the story of a legend.'},
+    {id: 15, title: 'Simba', body: 'the lion king with biggest heart.'},
+    {id: 16, title: 'Chilly', body: 'the cat with no boundaries.'},
+    {id: 17, title: 'Fish 1', body: 'just a simple gold fish.'},
+    {id: 18, title: 'Fish 2', body: "yet, another gold fish. <br> Tetraodontidae <u>are a family</u> of primarily marine and estuarine <b>fish</b></span>"},
+    {id: 19, title: 'Fish 3-6', body: 'Ready for combat.'},
+    {id: 20, title: 'The Russian fish', body: 'some say, he is waiting to clean us all.'}
+];
+
+module.exports = {
+    pages: PAGES
+};
