23d168eb856f8372b32e1d5dbce5675d9367feff
thebigr <nevoroy@gmail.com>
2018-09-29 10:26:01 +0300

    changed author changed server to work on rowiki collection and not test.

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://wikAdmin:' + process.env.MONGO_ATLAS_PW + '@royscluster-7svan.mongodb.net/test?retryWrites=true', {useNewUrlParser:true});
+mongoose.connect('mongodb+srv://wikAdmin:' + process.env.MONGO_ATLAS_PW + '@royscluster-7svan.mongodb.net/rowiki?retryWrites=true', {useNewUrlParser:true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
